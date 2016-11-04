47ad92f66ceb007e8c98aa68b8673a1255de89cc
andreucm <andreu@beta-robots.com>
2016-11-04 21:39:32 +0100

    First Version of arduino sine wave. All working ok!

---
+++ src/oscillator/oscillator.ino
@@ -0,0 +1,46 @@
+/****
+ * Oscillator
+ **/
+
+//includes
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+//ROS variables
+ros::NodeHandle nh_;
+std_msgs::Float32 float_msg_;
+ros::Publisher publisher_("wave", &float_msg_);
+
+//other variables
+float angle_ = 0; 
+const float PI_ = 3.141592;
+
+//setup 
+void setup()
+{ 
+    //ROS init
+    nh_.initNode();
+    nh_.advertise(publisher_);
+}
+
+void loop()
+{
+    //increment the angle
+    angle_ = angle_ + PI_/100; 
+    if (angle_ > 2*PI_) angle_ = angle_ - 2.0*PI_; 
+
+    //publish
+    float_msg_.data = cos(angle_); 
+    publisher_.publish(&float_msg_);
+
+    //spin (ros sync and attend callbacks, if any ...)
+    nh_.spinOnce();
+    
+    //relax (50ms)
+    delay(20);     
+}
